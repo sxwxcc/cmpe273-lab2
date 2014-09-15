1c284162689c05ad26bf3209f2bf2399fae759ce
sxwxcc <sxwxcc@gamil.com>
2014-09-15 12:43:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ var login = require('./login');
 
 var app = connect();
 
-app.use(connect.json()); // Parse JSON request body into `request.body`
-app.use(connect.urlencoded()); // Parse form in request body into `request.body`
-app.use(connect.cookieParser()); // Parse cookies in the request headers into `request.cookies`
-app.use(connect.query()); // Parse query string into `request.query`
+app.use(connect.json());
+app.use(connect.urlencoded());
+app.use(connect.cookieParser());
+app.use(connect.query());
 
 app.use('/', main);
 
@@ -22,43 +22,48 @@ function main(request, response, next) {
 function get(request, response) {
 	var cookies = request.cookies;
 	console.log(cookies);
-	if ('session_id' in cookies) {
-		var sid = cookies['session_id'];
-		if ( login.isLoggedIn(sid) ) {
-			response.setHeader('Set-Cookie', 'session_id=' + sid);
-			response.end(login.hello(sid));	
-		} else {
-			response.end("Invalid session_id! Please login again\n");
-		}
-	} else {
-		response.end("Please login via HTTP POST\n");
+	   if ('session_id' in cookies)
+      {
+	var sid = cookies['session_id'];
+        if ( login.isLoggedIn(sid) )
+        {
+	    response.setHeader('Set-Cookie', 'session_id=' + sid);
+	    response.end(login.hello(sid));	
 	}
-	app.use(function(req, res){
-  res.end('Hello from Connect!\n');})
+        else
+        {
+			response.end("Invalid session_id! Please login again\n");
+	}
+      }
+       else
+          {
+	         	response.end("Please login via HTTP POST\n");
+	  }
 };
 
 function post(request, response) {
-	// TODO: read 'name and email from the request.body'
-	// var newSessionId = login.login('xxx', 'xxx@gmail.com');
-	// TODO: set new session id to the 'session_id' cookie in the response
-	// replace "Logged In" response with response.end(login.hello(newSessionId));
-
-	response.end("Logged In\n");
+	var newSessionId = login.login('Nishant Jha', 'nishant.jha@sjsu.edu');
+        var cookies = request.cookies;
+        response.setHeader('Set-Cookie', 'session_id=' + newSessionId);
+	cookies['session_id'] = newSessionId;
+        response.end(login.hello(newSessionId));
 };
 
 function del(request, response) {
 	console.log("DELETE:: Logout from the server");
- 	// TODO: remove session id via login.logout(xxx)
- 	// No need to set session id in the response cookies since you just logged out!
-
+ 	var cookies=request.cookies;
+        var sid=cookies['session_id'];
+        login.logout(sid);
   	response.end('Logged out from the server\n');
 };
 
 function put(request, response) {
-	console.log("PUT:: Re-generate new seesion_id for the same user");
-	// TODO: refresh session id; similar to the post() function
-
-	response.end("Re-freshed session id\n");
+	console.log("PUT:: Re-generate new session_id for the same user");
+	var newSessionId = login.login('Nishant Jha', 'nishant.jha@sjsu.edu');
+        var cookies = request.cookies;
+        response.setHeader('Set-Cookie', 'session_id=' + newSessionId);
+        cookies['session_id'] = newSessionId;
+       	response.end("Re-freshed session id\n");
 };
 
 app.listen(8000);
